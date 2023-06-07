7774a1b9e5af2cf54201bbecd52181965515180a
Mike <mikelparlare@gmail.com>
2023-06-07 07:00:17 -0600

    clase 17

---
+++ Introucción/templateStrings.js
@@ -0,0 +1,6 @@
+const nombre = "Mikel IBK";
+const edad = 27;
+const pais = "México";
+
+console.log('La persona es ' +nombre+ 'tiene ' + edad + 'y es de ' +pais);
+console.log(`La persona es ${ nombre } tiene ${ edad } y es de ${ pais }`);
