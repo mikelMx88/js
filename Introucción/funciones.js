71f89b6881715dd61a8f048199eb11748116104f
Mike <mikelparlare@gmail.com>
2023-06-11 14:49:49 -0600

    clase 21

---
+++ Introucción/funciones.js
@@ -0,0 +1,15 @@
+// function saludo(){
+//     console.log('hola');
+// }
+
+// const variable = saludo;
+// saludo();
+// console.log(variable);
+
+// const saludo = function(){
+//     console.log('hola desde variable');
+// }
+
+// saludo();
+
+const saludo = () => console.log('Hola')
